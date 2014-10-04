b5ae0f4e3be2a0edff7eea28b21eea04f442aa8e
vagrant <vagrant@precise32.(none)>
2014-10-04 17:48:49 +0000

    hooked up quotes database

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Splurty::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
+  resources :quotes
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
