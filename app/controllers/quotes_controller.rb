0ec2057166057ad43ee507f99a285fe6640e8e7e
vagrant <vagrant@precise32.(none)>
2014-10-04 18:51:54 +0000

    added simple form and launched button

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,20 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").last
 end
+
+def new
+		@quote = Quote.new
+end
+def create
+	Quote.create(quote_params)
+	redirect_to quotes_path
+	end
+
+private
+
+def quote_params
+	params.require(:quote).permit(:saying, :author)
+
+end
+
 end
